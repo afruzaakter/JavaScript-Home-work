4c5d3ff53bf19eb2550b6ac7fb162c18b750c951
afruzaakter <afruzaaktersonia1995@gmail.com>
2022-01-31 19:35:40 +0600

    add conceptual session problem sulotion

---
+++ conceptual-Problem.js
@@ -0,0 +1,98 @@
+// problem-1: kono akta letter vowel na consonant ta check kora 
+
+// var input = "f";
+// var letter = input.toLocaleLowerCase();
+// if(letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u"){
+//     console.log(letter,"is vowel")
+// }
+// else{
+//     console.log(letter,'is consonant');
+// }
+
+// Problem-2: 3 ti number ar modde big number nirnoy koro
+// var number1 = 700;
+// var number2 = 900;
+// var number3 = 800;
+// if(number1>number2){
+//     if(number1>number3){
+//         console.log("Number1 is big")
+//     }
+// }
+// if(number2> number1){
+//     if(number2> number3){
+//         console.log("Number2 is big")
+//     }
+// }
+
+// else{
+//     console.log("Number3 is big")
+// }
+// Problem-2: 3 ta number ar modde small number nirnoy koro
+// var number1 = 50;
+// var number2 = 60;
+// var number3 = 10;
+// if(number1<number2){
+//     if(number1<number3){
+//         console.log("Number1 is small");
+//     }
+// }
+// if(number2<number3){
+//     if(number2<number1){
+//         console.log("Number2 is small")
+//     }
+// }
+// else{
+//     console.log("Number3 is small")
+// }
+
+
+// Problem-3: 1 theke 100 ar modde jor even number ber koro
+
+// process-1
+// for(var i = 2; i <=100; i++){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+// }
+// Process-2 while loop use kore 
+// vol process 
+// var i = 2;
+// while(i <= 100){
+//  if(i % 2 == 0){
+//      i++;
+//      console.log(i)
+//  }
+// }
+// Problem-3: 50 theke 80 ar modde jor odd/bijor number ber koro
+
+// for (var i = 50; i <= 80; i++ ){
+//     if(i % 2 == 1){
+//         console.log(i);
+//     }
+// }
+// Problem-3: 1 theke 100 ar modde jor odd/bijor number ber koro
+for (var i = 0; i <= 100; i++ ){
+    if(i % 2 == 1){
+        console.log(i);
+    }
+}
+
+
+
+// problem-4: loop ar use kore 7 gorer namota koro? 
+
+// Process-1:
+// var number = 7;
+// for(var i = 1; i <= 10; i++){
+//     var result = i * number;
+//     console.log(result);
+// }
+
+// Process-2:
+// var number = 13;
+// for( var i = 1; i <=10; i++){
+//     var result = i * number;
+//     var multiplicationTable = number + " * " + i + " = " + result;
+//     console.log(multiplicationTable);
+// }
+
